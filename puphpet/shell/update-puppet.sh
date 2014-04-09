25a9dafa8f63115d7b6ca414c117b834aede97b4
chris_hall_hu_cheng <chris_hall_hu_cheng@1580660.no-reply.drupal.org>
2014-04-09 01:39:48 -0500

    Provisioning fails since Puppet 3.5 update 2233977

--- puphpet/shell/update-puppet.sh
+++ puphpet/shell/update-puppet.sh
@@ -9,7 +9,7 @@ CODENAME=$(/bin/bash "${VAGRANT_CORE_FOLDER}/shell/os-detect.sh" CODENAME)
 if [[ ! -f /.puphpet-stuff/update-puppet ]]; then
     if [ "${OS}" == 'debian' ] || [ "${OS}" == 'ubuntu' ]; then
         echo "Downloading http://apt.puppetlabs.com/puppetlabs-release-${CODENAME}.deb"
-        wget --quiet --tries=5 --timeout=10 -O "/.puphpet-stuff/puppetlabs-release-${CODENAME}.deb" "http://apt.puppetlabs.com/puppetlabs-release-${CODENAME}.deb"
+        wget --quiet --tries=5 --connect-timeout=10 -O "/.puphpet-stuff/puppetlabs-release-${CODENAME}.deb" "http://apt.puppetlabs.com/puppetlabs-release-${CODENAME}.deb"
         echo "Finished downloading http://apt.puppetlabs.com/puppetlabs-release-${CODENAME}.deb"
 
         dpkg -i "/.puphpet-stuff/puppetlabs-release-${CODENAME}.deb" >/dev/null
@@ -18,10 +18,9 @@ if [[ ! -f /.puphpet-stuff/update-puppet ]]; then
         apt-get update >/dev/null
         echo "Finished running update-puppet apt-get update"
 
-        echo "Updating Puppet to latest version"
-        apt-get -y install puppet >/dev/null
-        PUPPET_VERSION=$(puppet help | grep 'Puppet v')
-        echo "Finished updating puppet to latest version: ${PUPPET_VERSION}"
+        echo "Updating Puppet to version 3.4.3"
+        apt-get install -y puppet=3.4.3-1puppetlabs1 puppet-common=3.4.3-1puppetlabs1 >/dev/null
+        echo "Finished updating puppet to version: 3.4.3"
 
         touch /.puphpet-stuff/update-puppet
         echo "Created empty file /.puphpet-stuff/update-puppet"
